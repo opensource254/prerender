feda8dde69471cc907fc427f19ab447910273e52
Stanley Masinde <stanmasinde@gmail.com>
2020-07-03 16:54:43 +0300

    The ability to cache pages after a request, This cache is currently useless

--- services/prerender.js
+++ services/prerender.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 const config = require("../config");
-const { enabled } = require('../app');
+const fs = require('fs')
 
 class PrerenderService {
     url
+    directory
     constructor(url = 'https://example.org') {
         this.url = url
     }
@@ -12,8 +13,32 @@ class PrerenderService {
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
         await page.goto(this.url)
-        page.setJavaScriptEnabled(true)
+        this.addPageToCache(page, browser)
         return page.content()
+    }
+
+    /**
+     * Create a slug
+     * @param {String} title 
+     * @returns String
+     */
+    createTitleSlug(title = '') {
+        return title.split(' ').join('_').toLowerCase()
+    }
+
+    /**
+     * Add the fetched rendered page to cache
+     * @param {Array} page 
+     */
+    async addPageToCache(page, browser) {
+        const directory = new URL(this.url).hostname
+        fs.mkdir(`./output/${directory}`, (err) => {
+            console.warn(err);
+        })
+        const titleSlug = this.createTitleSlug(await page.title())
+        fs.writeFileSync(`./output/${directory}/${titleSlug}.html`, (await page.content()), { encoding: "utf-8" })
+
+        await browser.close()
     }
 }
 
